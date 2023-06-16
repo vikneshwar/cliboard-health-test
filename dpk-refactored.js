d724579467d974c11325be237869e59597a59f01
vikneshwar <lviknesh1993@gmail.com>
2023-06-16 13:27:04 +0530

    refactor to use *if* instead of ternary

--- dpk-refactored.js
+++ dpk-refactored.js
@@ -2,6 +2,7 @@ const crypto = require("crypto");
 
 const HASH_ALGORITHM = "sha3-512";
 const DIGEST = "hex";
+
 const generateCandidateHash = (
   data,
   { algorithm = HASH_ALGORITHM, digest = DIGEST } = {}
@@ -14,20 +15,27 @@ exports.deterministicPartitionKey = (event) => {
   let candidate;
 
   if (!event) {
-    candidate = TRIVIAL_PARTITION_KEY;
-    return candidate;
+    return TRIVIAL_PARTITION_KEY;
   }
 
   candidate =
     event.partitionKey ?? generateCandidateHash(JSON.stringify(event));
 
-  candidate =
-    typeof candidate !== "string" ? JSON.stringify(candidate) : candidate;
+  /* candidate =
+    typeof candidate !== "string" ? JSON.stringify(candidate) : candidate; */
 
-  candidate =
+  if(typeof candidate !== "string") {
+    candidate = JSON.stringify(candidate);
+  }
+
+  /* candidate =
     candidate.length > MAX_PARTITION_KEY_LENGTH
       ? generateCandidateHash(candidate)
-      : candidate;
+      : candidate; */
+
+  if(candidate.length > MAX_PARTITION_KEY_LENGTH) {
+    candidate = generateCandidateHash(candidate);
+  }
 
   return candidate;
 };
